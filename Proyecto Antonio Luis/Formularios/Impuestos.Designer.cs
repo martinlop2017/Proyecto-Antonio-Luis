7c9d26a184baabb08cd73ec3e8ddc10a1cf69c81
Martin <Martin@MARTIN-PC>
2017-10-06 16:38:00 +0200

    Diseño Formuilario Impuestos

---
+++ Proyecto Antonio Luis/Formularios/Impuestos.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Proyecto_Antonio_Luis.Formularios
+{
+    partial class Impuestos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Impuestos));
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.dataGridViewIVAs = new System.Windows.Forms.DataGridView();
+            this.label15 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIVAs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonAceptar.BackgroundImage")));
+            this.buttonAceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonAceptar.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.buttonAceptar.FlatAppearance.BorderSize = 0;
+            this.buttonAceptar.Location = new System.Drawing.Point(129, 412);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(208, 60);
+            this.buttonAceptar.TabIndex = 114;
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewIVAs
+            // 
+            this.dataGridViewIVAs.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewIVAs.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewIVAs.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewIVAs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewIVAs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewIVAs.Location = new System.Drawing.Point(40, 115);
+            this.dataGridViewIVAs.Name = "dataGridViewIVAs";
+            this.dataGridViewIVAs.RowHeadersWidth = 15;
+            this.dataGridViewIVAs.Size = new System.Drawing.Size(378, 255);
+            this.dataGridViewIVAs.TabIndex = 117;
+            this.dataGridViewIVAs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewIVAs_CellContentClick);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Tahoma", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label15.Location = new System.Drawing.Point(78, 31);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(310, 42);
+            this.label15.TabIndex = 118;
+            this.label15.Text = "Tipos Impuestos";
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 226.7426F;
+            this.Column1.HeaderText = "Tipo";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 83.29713F;
+            this.Column2.HeaderText = "I.V.A.";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 79.80803F;
+            this.Column3.HeaderText = "R.E.";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 10.15228F;
+            this.Column4.HeaderText = " ";
+            this.Column4.Name = "Column4";
+            // 
+            // Impuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(465, 528);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.dataGridViewIVAs);
+            this.Controls.Add(this.buttonAceptar);
+            this.Name = "Impuestos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Impuestos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIVAs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.DataGridView dataGridViewIVAs;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
---
+++ Proyecto Antonio Luis/Formularios/Impuestos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Antonio_Luis.Formularios
+{
+    public partial class Impuestos : Form
+    {
+        public Impuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridViewIVAs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
