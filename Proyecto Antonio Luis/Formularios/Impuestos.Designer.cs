406685411d5f54dbf0847cb1e30c7f56b580d832
Martin <Martin@MARTIN-PC>
2017-10-17 19:19:52 +0200

    Trabajo sobre el formulario de impuestos

--- Proyecto Antonio Luis/Formularios/Impuestos.Designer.cs
+++ Proyecto Antonio Luis/Formularios/Impuestos.Designer.cs
@@ -28,15 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Impuestos));
             this.buttonAceptar = new System.Windows.Forms.Button();
             this.dataGridViewIVAs = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label15 = new System.Windows.Forms.Label();
+            this.ivaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ususariosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.salir = new System.Windows.Forms.Button();
+            this.modificar = new System.Windows.Forms.Button();
+            this.eliminar = new System.Windows.Forms.Button();
+            this.sumar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIVAs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ivaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ususariosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonAceptar
@@ -45,7 +56,7 @@
             this.buttonAceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonAceptar.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
             this.buttonAceptar.FlatAppearance.BorderSize = 0;
-            this.buttonAceptar.Location = new System.Drawing.Point(129, 412);
+            this.buttonAceptar.Location = new System.Drawing.Point(56, 411);
             this.buttonAceptar.Name = "buttonAceptar";
             this.buttonAceptar.Size = new System.Drawing.Size(208, 60);
             this.buttonAceptar.TabIndex = 114;
@@ -57,52 +68,147 @@
             this.dataGridViewIVAs.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.dataGridViewIVAs.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridViewIVAs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewIVAs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4});
-            this.dataGridViewIVAs.Location = new System.Drawing.Point(40, 115);
+            this.dataGridViewIVAs.DataBindings.Add(new System.Windows.Forms.Binding("Tag", this.ivaBindingSource, "numerador", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.dataGridViewIVAs.Location = new System.Drawing.Point(46, 210);
             this.dataGridViewIVAs.Name = "dataGridViewIVAs";
             this.dataGridViewIVAs.RowHeadersWidth = 15;
-            this.dataGridViewIVAs.Size = new System.Drawing.Size(378, 255);
+            this.dataGridViewIVAs.Size = new System.Drawing.Size(455, 165);
             this.dataGridViewIVAs.TabIndex = 117;
             this.dataGridViewIVAs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewIVAs_CellContentClick);
             // 
-            // Column1
+            // ivaBindingSource
             // 
-            this.Column1.FillWeight = 226.7426F;
-            this.Column1.HeaderText = "Tipo";
-            this.Column1.Name = "Column1";
+            this.ivaBindingSource.DataSource = typeof(BaseDatos.Iva);
             // 
-            // Column2
+            // ususariosBindingSource
             // 
-            this.Column2.FillWeight = 83.29713F;
-            this.Column2.HeaderText = "I.V.A.";
-            this.Column2.Name = "Column2";
+            this.ususariosBindingSource.DataSource = typeof(BaseDatos.Ususarios);
             // 
-            // Column3
+            // button1
             // 
-            this.Column3.FillWeight = 79.80803F;
-            this.Column3.HeaderText = "R.E.";
-            this.Column3.Name = "Column3";
+            this.button1.Location = new System.Drawing.Point(290, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 30);
+            this.button1.TabIndex = 120;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Column4
+            // label7
             // 
-            this.Column4.FillWeight = 10.15228F;
-            this.Column4.HeaderText = " ";
-            this.Column4.Name = "Column4";
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(153)))), ((int)(((byte)(208)))));
+            this.label7.Location = new System.Drawing.Point(122, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(310, 42);
+            this.label7.TabIndex = 131;
+            this.label7.Text = "Tipos Impuestos";
             // 
-            // label15
+            // label5
             // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Tahoma", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label15.Location = new System.Drawing.Point(78, 31);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(310, 42);
-            this.label15.TabIndex = 118;
-            this.label15.Text = "Tipos Impuestos";
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(475, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 130;
+            this.label5.Text = "&Salir";
+            this.label5.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(155, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "&Modificar";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(102, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 127;
+            this.label2.Text = "&Eliminar";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(45, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 126;
+            this.label1.Text = "&Añadir";
+            this.label1.Visible = false;
+            // 
+            // salir
+            // 
+            this.salir.BackColor = System.Drawing.Color.White;
+            this.salir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("salir.BackgroundImage")));
+            this.salir.Location = new System.Drawing.Point(463, 105);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(52, 56);
+            this.salir.TabIndex = 125;
+            this.salir.UseVisualStyleBackColor = false;
+            this.salir.MouseEnter += new System.EventHandler(this.salir_MouseEnter);
+            this.salir.MouseLeave += new System.EventHandler(this.salir_MouseLeave);
+            // 
+            // modificar
+            // 
+            this.modificar.BackColor = System.Drawing.Color.White;
+            this.modificar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("modificar.BackgroundImage")));
+            this.modificar.Location = new System.Drawing.Point(154, 105);
+            this.modificar.Name = "modificar";
+            this.modificar.Size = new System.Drawing.Size(52, 56);
+            this.modificar.TabIndex = 123;
+            this.modificar.UseVisualStyleBackColor = false;
+            this.modificar.MouseEnter += new System.EventHandler(this.modificar_MouseEnter);
+            this.modificar.MouseLeave += new System.EventHandler(this.modificar_MouseLeave);
+            // 
+            // eliminar
+            // 
+            this.eliminar.BackColor = System.Drawing.Color.White;
+            this.eliminar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("eliminar.BackgroundImage")));
+            this.eliminar.Location = new System.Drawing.Point(96, 105);
+            this.eliminar.Name = "eliminar";
+            this.eliminar.Size = new System.Drawing.Size(52, 56);
+            this.eliminar.TabIndex = 122;
+            this.eliminar.UseVisualStyleBackColor = false;
+            this.eliminar.MouseEnter += new System.EventHandler(this.eliminar_MouseEnter);
+            this.eliminar.MouseLeave += new System.EventHandler(this.eliminar_MouseLeave);
+            // 
+            // sumar
+            // 
+            this.sumar.BackColor = System.Drawing.Color.White;
+            this.sumar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("sumar.BackgroundImage")));
+            this.sumar.Location = new System.Drawing.Point(38, 105);
+            this.sumar.Name = "sumar";
+            this.sumar.Size = new System.Drawing.Size(52, 56);
+            this.sumar.TabIndex = 121;
+            this.sumar.UseVisualStyleBackColor = false;
+            this.sumar.MouseEnter += new System.EventHandler(this.sumar_MouseEnter);
+            this.sumar.MouseLeave += new System.EventHandler(this.sumar_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new System.Drawing.Point(290, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 60);
+            this.button2.TabIndex = 197;
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Impuestos
             // 
@@ -111,8 +217,18 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(465, 528);
-            this.Controls.Add(this.label15);
+            this.ClientSize = new System.Drawing.Size(549, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.modificar);
+            this.Controls.Add(this.eliminar);
+            this.Controls.Add(this.sumar);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridViewIVAs);
             this.Controls.Add(this.buttonAceptar);
             this.DoubleBuffered = true;
@@ -121,6 +237,8 @@
             this.Text = "Impuestos";
             this.Load += new System.EventHandler(this.Impuestos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIVAs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ivaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ususariosBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,10 +248,18 @@
 
         private System.Windows.Forms.Button buttonAceptar;
         private System.Windows.Forms.DataGridView dataGridViewIVAs;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        public System.Windows.Forms.BindingSource ivaBindingSource;
+        private System.Windows.Forms.BindingSource ususariosBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button salir;
+        private System.Windows.Forms.Button modificar;
+        private System.Windows.Forms.Button eliminar;
+        private System.Windows.Forms.Button sumar;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Proyecto Antonio Luis/Formularios/Impuestos.cs
+++ Proyecto Antonio Luis/Formularios/Impuestos.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BaseDatos;
+using System.Data.SqlClient;
+
+
 
 namespace Proyecto_Antonio_Luis.Formularios
 {
@@ -30,14 +33,109 @@ namespace Proyecto_Antonio_Luis.Formularios
 
         private void Impuestos_Load(object sender, EventArgs e)
         {
-            // baseDeDatos.Ususarios.Select(user => user.nombre).ToList<string>();
-            //   dataGridViewIVAs.Column[1].Name = "hola";
-            //BaseDatos.Iva.Select(User => user.icaconcepto).ToList<string>();
-            //          dataGridViewIVAs.DataSource = baseDeDatos.Ususarios.Select(user => user.nombre).ToList<string>();
 
-            dataGridViewIVAs.DataSource = baseDeDatos.Iva.Select(user=> user.ivaconcepto).ToList<string>();
-          // dataGridViewIVAs.Columns[0] = baseDeDatos.Iva.Select(user => user.ivaconcepto).ToList<string>();
+
+            dataGridViewIVAs.DataSource = baseDeDatos.Iva.ToList();
+
+
+
+
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            var nuevoiva = new Iva();
+            nuevoiva.ivaconcepto = "nuevoiva concpeto";
+            nuevoiva.ivaporciento = 16;
+
+  
+
+
+            baseDeDatos.Iva.Add(nuevoiva);
+
+
+            // Guardamos los cambios
+          
+            baseDeDatos.SaveChanges();
+    
+            
+
+
+        }
+
+
+        //************************************************************************************************************
+        //************************************************************************************************************
+        //************************************************************************************************************
+        //*****************                                                                              *************
+        //*****************        Los eventos del mouse sobre los botones para mostrar las etiquetas    *************
+        //*****************                                                                              *************
+        //************************************************************************************************************
+        //************************************************************************************************************
+        //************************************************************************************************************
+
+
+        private void sumar_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void sumar_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void eliminar_MouseEnter(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+        }
+
+        private void eliminar_MouseLeave(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+        }
+
+        private void modificar_MouseEnter(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+        }
+
+        private void modificar_MouseLeave(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+        }
+
+        private void salir_MouseEnter(object sender, EventArgs e)
+        {
+            label5.Visible = true;
+        }
+
+        private void salir_MouseLeave(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+        }
+
+        //============================================================================================================
+        //============================================================================================================
+        //============================================================================================================
+        //=================                                                                               ============
+        //=================                                                                               ============
+        //=================      Aquí termina las acciones al pulsar los distintos botones               ============
+        //=================                                                                               ============
+        //=================                                                                               ============
+        //============================================================================================================
+        //============================================================================================================
+        //============================================================================================================
+
+
+
+
+
+
+
     }
 }
